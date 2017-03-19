1bdbdb58c613daae341fa65113c44cb3a7ef6e65
WEISU <jacketsu@hotmail.com>
2017-03-18 22:18:41 -0400

    210

---
+++ 210.Course_Schedule_II.java
@@ -0,0 +1,66 @@
+// There are a total of n courses you have to take, labeled from 0 to n - 1.
+
+// Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+// Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+
+// There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+
+// For example:
+
+// 2, [[1,0]]
+// There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1]
+
+// 4, [[1,0],[2,0],[3,1],[3,2]]
+// There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. So one correct course order is [0,1,2,3]. Another correct ordering is[0,2,1,3].
+
+
+public class Solution {
+    /**
+     * @param numCourses a total of n courses
+     * @param prerequisites a list of prerequisite pairs
+     * @return the course order
+     */
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        // Write your code here
+        List[] edges = new ArrayList[numCourses];   //List Array;
+        int[] degree = new int[numCourses];
+        
+        for (int i = 0; i < numCourses; i++) {
+            edges[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            degree[prerequisites[i][0]]++;
+            edges[prerequisites[i][1]].add(prerequisites[i][0]);
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (degree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        
+        int[] order = new int[numCourses];
+        int count = 0;
+        while(!queue.isEmpty()) {
+            int course = (int)queue.poll();
+            order[count] = course;
+            count++;
+            int n = edges[course].size();
+            for (int i = 0; i < n; i++) {
+                int pointer = (int)edges[course].get(i);
+                degree[pointer]--;
+                if (degree[pointer] == 0) {
+                    queue.offer(pointer);
+                }
+            }
+        }
+        
+        if (count == numCourses) {
+            return order;
+        }
+        
+        return new int[0];
+    }
+}
