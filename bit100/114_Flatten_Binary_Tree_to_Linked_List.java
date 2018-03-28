1b8eef5f96810f27e6684a4f7b6fb0b19a589ddd
WEISU <jacketsu@hotmail.com>
2018-03-27 20:42:14 -0400

    114

---
+++ bit100/114_Flatten_Binary_Tree_to_Linked_List.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    TreeNode pre = null;
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        flatten(root.right);
+        flatten(root.left);
+        TreeNode tmp = root.left;
+        root.left = null;
+        root.right = pre;
+        pre = root;
+    }
+}
