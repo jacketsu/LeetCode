e96f3cd138213d20995471a8a3d92cb1b6365b81
WEISU <jacketsu@hotmail.com>
2018-04-01 12:46:49 -0400

    218

---
+++ bit100/213_House Robber II.java
@@ -0,0 +1,32 @@
+// Note: This is an extension of House Robber.
+
+// After robbing those houses on that street, the thief has found himself a new place for his thievery so that he will not get too much attention. This time, all houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, the security system for these houses remain the same as for those in the previous street.
+
+// Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+// Credits:
+// Special thanks to @Freezen for adding this problem and creating all test cases.
+// 
+// 
+
+
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        return Math.max(robHelper(nums, 0, nums.length - 2), robHelper(nums, 1, nums.length - 1));
+    }
+    
+    public int robHelper(int[] nums, int lo, int hi) {
+        int preNotRob = 0, preRob = 0, notRob = 0, rob = 0;
+        while (lo <= hi) {
+            rob = preNotRob + nums[lo];
+            notRob = Math.max(preNotRob, preRob);
+            preNotRob = notRob;
+            preRob = rob;
+            lo++;
+        }
+        
+        return Math.max(notRob, rob);
+    }
+}
