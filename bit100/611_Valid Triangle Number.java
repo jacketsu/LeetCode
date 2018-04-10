04c234d1519118ded7d702234a6aa20cb2556556
WEISU <jacketsu@hotmail.com>
2018-04-09 21:19:49 -0400

    611

---
+++ bit100/611_Valid Triangle Number.java
@@ -0,0 +1,35 @@
+// Given an array consists of non-negative integers, your task is to count the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+// Example 1:
+// Input: [2,2,3,4]
+// Output: 3
+// Explanation:
+// Valid combinations are: 
+// 2,3,4 (using the first 2)
+// 2,3,4 (using the second 2)
+// 2,2,3
+// Note:
+// The length of the given array won't exceed 1000.
+// The integers in the given array are in the range of [0, 1000].
+// 
+// 
+
+
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0;
+        for (int k = nums.length - 1; k >= 2; k--) {
+            int i = 0, j = k - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] > nums[k]) {
+                    count += (j - i);
+                    j--;
+                    // i = 0;               no need...
+                } else {
+                    i++;
+                }
+            }
+        }
+        return count;
+    }
+}
