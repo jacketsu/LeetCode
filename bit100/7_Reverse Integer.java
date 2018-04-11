a98a98d02e23326de6ba7f2d416a462a705bb212
WEISU <jacketsu@hotmail.com>
2018-04-10 22:18:53 -0400

    p

---
+++ bit100/7_Reverse Integer.java
@@ -0,0 +1,31 @@
+// Given a 32-bit signed integer, reverse digits of an integer.
+
+// Example 1:
+
+// Input: 123
+// Output:  321
+// Example 2:
+
+// Input: -123
+// Output: -321
+// Example 3:
+
+// Input: 120
+// Output: 21
+// Note:
+// Assume we are dealing with an environment which could only hold integers within the 32-bit signed integer range. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+class Solution {
+    public int reverse(int x) {
+        long res = 0L;
+        boolean negative = x < 0;
+        x = Math.abs(x);                                    //abs
+        while (x != 0) {
+            res = res * 10 + (x % 10);
+            x /= 10;
+        }
+        if (negative) res *= -1;
+        if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) return 0;
+        return (int)res;
+    }
+}
---
+++ bit100/9_Palindrome Number.java
@@ -0,0 +1,16 @@
+// Determine whether an integer is a palindrome. Do this without extra space.
+// 
+
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+        int rev = 0;
+        while (rev < x) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return (x == rev || x == (rev / 10));
+    }
+}
