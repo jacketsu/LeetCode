3b9a6d91e1e4b2f8c934dad7fdfb0f5dbee8dae1
WEISU <jacketsu@hotmail.com>
2018-04-25 11:27:19 -0400

    81

---
+++ bit100/81_Search in Rotated Sorted Array II.java
@@ -0,0 +1,51 @@
+// Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+// (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+
+// You are given a target value to search. If found in the array return true, otherwise return false.
+
+// Example 1:
+
+// Input: nums = [2,5,6,0,0,1,2], target = 0
+// Output: true
+// Example 2:
+
+// Input: nums = [2,5,6,0,0,1,2], target = 3
+// Output: false
+// Follow up:
+
+// This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
+// Would this affect the run-time complexity? How and why?
+
+
+class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return false;
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[start] == nums[mid] && nums[end] == nums[mid]) {           //deal with the duplicates...
+                start++;
+                end--;
+            } else if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target <= nums[mid]) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[end]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+        if (nums[start] == target) return true;
+        if (nums[end] == target) return true;
+        return false;
+    }
+}
