ca0dc252399e7de7b8cf0b12cabeda6c8bbfa6de
WEISU <jacketsu@hotmail.com>
2018-05-01 11:25:48 -0400

    294

---
+++ bit100/294_Flip Game II.java
@@ -0,0 +1,22 @@
+// You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
+
+// Write a function to determine if the starting player can guarantee a win.
+
+// For example, given s = "++++", return true. The starting player can guarantee a win by flipping the middle "++" to become "+--+".
+
+// Follow up:
+// Derive your algorithm's runtime complexity.
+
+
+class Solution {
+    public boolean canWin(String s) {
+        if (s == null || s.length() < 2) return false;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '+' && s.charAt(i - 1) == '+') {
+                String str = s.substring(0, i - 1) + "--" + s.substring(i + 1, s.length());
+                if (!canWin(str)) return true;
+            }
+        }
+        return false;
+    }
+}
