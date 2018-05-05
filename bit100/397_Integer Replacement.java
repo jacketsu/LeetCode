f49a57c9dbae3d3252dfc7fe64207dec1ef1ecbb
WEISU <jacketsu@hotmail.com>
2018-05-05 01:15:56 -0400

    397

---
+++ bit100/397_Integer Replacement.java
@@ -0,0 +1,48 @@
+// Given a positive integer n and you can do operations as follow:
+
+// If n is even, replace n with n/2.
+// If n is odd, you can replace n with either n + 1 or n - 1.
+// What is the minimum number of replacements needed for n to become 1?
+
+// Example 1:
+
+// Input:
+// 8
+
+// Output:
+// 3
+
+// Explanation:
+// 8 -> 4 -> 2 -> 1
+// Example 2:
+
+// Input:
+// 7
+
+// Output:
+// 4
+
+// Explanation:
+// 7 -> 8 -> 4 -> 2 -> 1
+// or
+// 7 -> 6 -> 3 -> 2 -> 1
+
+class Solution {
+    public int integerReplacement(int n) {
+        long ln = n;
+        int res = 0;
+        while (ln != 1) {
+            if (ln % 2 == 0) {
+                ln >>= 1;
+                res++;
+            } else if (ln == 3) {
+                res += 2;
+                break;
+            } else {
+                ln = (ln & 2) == 0 ? ln - 1 : ln + 1;
+                res++;
+            }
+        }
+        return res;
+    }
+}
