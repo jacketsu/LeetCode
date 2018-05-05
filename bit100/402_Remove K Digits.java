ac9ebda149d13c3e7c7d6bfa733f8178f5d559f6
WEISU <jacketsu@hotmail.com>
2018-05-05 17:08:28 -0400

    402

---
+++ bit100/402_Remove K Digits.java
@@ -0,0 +1,45 @@
+// Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+
+// Note:
+// The length of num is less than 10002 and will be ≥ k.
+// The given num does not contain any leading zero.
+// Example 1:
+
+// Input: num = "1432219", k = 3
+// Output: "1219"
+// Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+// Example 2:
+
+// Input: num = "10200", k = 1
+// Output: "200"
+// Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+// Example 3:
+
+// Input: num = "10", k = 2
+// Output: "0"
+// Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+
+class Solution {
+    public String removeKdigits(String num, int k) {
+        int digits = num.length() - k;
+        char[] stk = new char[num.length()];
+        int top = 0;
+        for (int i = 0; i < num.length(); i++) {
+            char c = num.charAt(i);
+            while (k > 0 && top > 0 && stk[top - 1] > c) {
+                top--;
+                k--;
+            }
+            stk[top++] = c;
+        }
+        
+        int index = 0;
+        while (index < digits && stk[index] == '0') {
+            index++;
+        }
+        if (index == digits) {
+            return "0";
+        }
+        return new String(stk, index, digits - index);
+    }
+}
